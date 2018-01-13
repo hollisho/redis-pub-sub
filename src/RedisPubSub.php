79c64e0c66e733e4a3816daeff55f50a85b612fd
Hollis_Ho <he_wenzhi@126.com>
2018-01-13 12:52:14 +0800

    Update RedisPubSub.php

--- src/RedisPubSub.php
+++ src/RedisPubSub.php
@@ -6,12 +6,25 @@ namespace RedisPubSub;
 use RedisPubSub\Client\RedisClient;
 use RedisPubSub\Exception\RedisException;
 
+/**
+ * Class RedisPubSub
+ * @package RedisPubSub
+ * @author Hollis Ho
+ */
 class RedisPubSub
 {
+    /* @var $_client \Redis */
+    private $_client;
+
+    public function __construct(\Redis $redis)
+    {
+        $this->_client = $redis;
+    }
+
     public function __call($name, $params)
     {
         try {
-            return call_user_func_array([$this->redis, $name], $params);
+            return call_user_func_array([$this->_client, $name], $params);
         } catch (\Exception $e) {
             var_dump($e->getMessage());
             throw new RedisException($e->getMessage());
@@ -19,15 +32,29 @@ class RedisPubSub
     }
 
     /**
+     * 只能通过静态方法获得该类的对象(单例模式)
+     * @author Hollis Ho <he_wenzhi@126.com>
+     * @param array $options
+     * @return null|\Redis
+     */
+    public static function getInstance($options = [])
+    {
+        static $obj = null;
+        if ($obj == null) {
+            $obj_tmp = new self($options);
+            $obj = $obj_tmp->_client;
+        }
+        return $obj;
+    }
+
+    /**
      * 发布消息
      * @param string $channel
      * @param string $message
      */
-    public static function publish($channel, $message)
+    public function publish($channel, $message)
     {
-        /* @var $redis \Redis */
-        $redis = RedisClient::getInstance();
-        $redis->publish($channel, $message);
+        $this->_client->publish($channel, $message);
     }
 
     /**
@@ -35,10 +62,8 @@ class RedisPubSub
      * @param array $channelArr
      * @param array $callback
      */
-    public static function subscribe($channelArr = [], $callback)
+    public function subscribe($channelArr = [], $callback)
     {
-        /* @var $redis \Redis */
-        $redis = RedisClient::getInstance();
-        $redis->subscribe($channelArr, $callback);
+        $this->_client->subscribe($channelArr, $callback);
     }
 }
